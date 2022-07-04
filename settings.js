0bcd0ef0408acdb8aad447e7018731f8611d33c5
kushanpramod <100111389+kushanpramod@users.noreply.github.com>
2022-07-04 15:39:06 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,15 +19,15 @@ global.APIKeys = {
 
 //customize settings\\
 
-global.alivemsg = `My name is RED DRAGON🔥
+global.alivemsg = `My name is 𝙺𝚒𝚗𝚐 𝙺𝙿 𝙱𝚘𝚝 2022 🇱🇰
 My Creator is Kumuthu Prabhasha😎` //Costomize Alive Message (type your message in `` )
 
 global.owner = ['+380944814219'] //Owner number in aive msg
 global.premium = ['+380944814219'] //Owner Number info
 global.ownernomer = '+380944814219' //Owner Number <<<
 
-global.ownername = '😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖' //Owner Name
-global.botname = ' ☬RED༒DRAGON' //Bot Name
+global.ownername = '😎⃝☠☜׀ˣ☬ I♥ αм   ⃟ᴷᵁˢᴴᴬᴺ𒆜⃝♚' //Owner Name
+global.botname = ' ☬𝐊𝐢𝐧𝐠 𝐊𝐏 𝐁𝐨𝐭 2022 🇱🇰༒' //Bot Name
 
 global.button = '🤭Youtube📍' //Costomize A Button Name In Alive Message
 global.btnurl = 'https://youtu.be/E5C-lrl_g9I' //Change Your Button's URL
@@ -35,7 +35,7 @@ global.btnurl = 'https://youtu.be/E5C-lrl_g9I' //Change Your Button's URL
 global.region = 'India, South India, Kerala' //Country Region
 
 global.packname = '☬RED༒DRAGON࿐' //Bot Pacage Name
-global.author = '😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖❯❯' //Author Name
+global.author = '😎𝗞𝘂𝘀𝗵𝗮𝗻 𝗣𝗿𝗮𝗺𝗼𝗱❯❯' //Author Name
 global.prefa = ['','!','.','😂','❤','💠'] //Command Prefix eg:(.alive ,alive 😂alive)
 global.sessionName = 'session' //Session Name!
 global.sp = '⭔' //A Charactor
